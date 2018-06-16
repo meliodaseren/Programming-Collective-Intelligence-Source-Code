f297c863f1a2443a5d89289ee44de2d1e452b5a7
meliodaseren-MacOSX <julian07027@gmail.com>
2018-06-16 22:13:42 +0800

    Modify shell

--- 2to3.6.sh
+++ 2to3.6.sh
@@ -7,8 +7,4 @@ cp -rf Ch* py2/;
 for folder in $(cat folderlist); do
     #$tool -wn $folder/*.py -o 2to3.6/$folder;
     $tool -w $folder/*.py;
-    #mv 2to3.6/$folder/*.py $folder/*.py;
 done
-
-
-
