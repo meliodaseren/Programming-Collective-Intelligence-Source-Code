b6ef9467922858bec25307483ec3b935e367dc95
meliodaseren-MacOSX <julian07027@gmail.com>
2018-06-16 22:13:26 +0800

    Translate python 2 -> 3.6, and backup python 2 codes

---
+++ 2to3.6.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+tool=$(which 2to3-3.6);
+
+cp -rf Ch* py2/;
+
+for folder in $(cat folderlist); do
+    #$tool -wn $folder/*.py -o 2to3.6/$folder;
+    $tool -w $folder/*.py;
+    #mv 2to3.6/$folder/*.py $folder/*.py;
+done
+
+
+
